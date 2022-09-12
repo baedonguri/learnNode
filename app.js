c8f8f3d60883c3255e2ebd6d2ccfead238729f79
baedonguri <rmsdbraos21@naver.com>
2022-09-12 20:13:03 +0900

    seperate routes

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 const http = require("http");
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended : false}));
 
-app.use('/add-product',(req, res, next)=> {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button>Add Product</button></form>');
-});
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
-app.use('/product', (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+
 })
-
-app.use('/',(req, res, next)=> {
-    res.send('<h1>Hello World</h1>');
-});
-
 
 app.listen(3001);
