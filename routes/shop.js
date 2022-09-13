56f558e2db4e469b3b0660f3f00af5531b0382c9
baedonguri <rmsdbraos21@naver.com>
2022-09-14 02:00:49 +0900

    apply template engine(pug)

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,16 @@
 const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
 
+
+
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname,'../' ,'views', 'shop.html'));
+    res.render('shop');
 })
 
 module.exports = router;
